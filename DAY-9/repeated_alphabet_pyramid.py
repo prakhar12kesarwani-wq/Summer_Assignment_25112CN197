964e6077b0836e37dab0140f6b59b158ac3a4661
Prakhar Kesarwani <prakhar.12kesarwani@gmail.com>
2026-06-09 15:08:29 +0530

    DAY-9 Uploaded

---
+++ DAY-9/repeated_alphabet_pyramid.py
@@ -0,0 +1,5 @@
+#repeated alphabet pyramid
+for i in range(1,6):
+    for j in range(1,i+1): #nested loop
+            print(chr(64+i),end="")   #converting integer value to char
+    print("")          #changes the line
---
+++ DAY-9/reverse_number_triangle.py
@@ -0,0 +1,7 @@
+for i in range(1,6):
+    k=1
+    for j in range(i,6):
+        print(k,end=" ")
+        k=k+1
+
+    print()    
