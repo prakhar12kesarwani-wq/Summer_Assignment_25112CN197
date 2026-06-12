81cd3667b1fd0314157e5524c5bfb7afef1ff51e
Prakhar Kesarwani <prakhar.12kesarwani@gmail.com>
2026-06-12 14:32:45 +0530

    DAY-10 Uploaded

---
+++ DAY-10/character_pyramid.py
@@ -0,0 +1,12 @@
+n=6
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end=" ")
+
+    for j in range(1,i+1):#ascending
+        print(chr(64+j),end=" ")    
+
+    for j in range(i-1,0,-1): #descending
+        print(chr(64+j),end=" ")    
+
+    print()    
---
+++ DAY-10/number_pyramid.py
@@ -0,0 +1,12 @@
+n=6
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end=" ")
+
+    for j in range(1,i+1):#ascending
+        print(j,end=" ")    
+
+    for j in range(i-1,0,-1): #descending
+        print(j,end=" ")    
+
+    print()    
